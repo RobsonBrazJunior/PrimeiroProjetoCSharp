b661b28100f898f2fc1f322dc3c8054f977f7d6c
Junior Braz <robsonbrazjunior@gmail.com>
2018-09-03 17:16:16 -0400

    Testando variaveis do tipo int

--- 2-CriandoVariaveis/Program.cs
+++ 2-CriandoVariaveis/Program.cs
@@ -11,6 +11,23 @@ namespace _2_CriandoVariaveis
         static void Main(string[] args)
         {
             Console.WriteLine("Executando projeto 2 - Criando Variaveis");
+
+            int idade;
+            idade = 32;
+            Console.WriteLine(idade);
+
+            idade = 10;
+            Console.WriteLine(idade);
+
+            idade = 10 + 5;
+            Console.WriteLine(idade);
+
+            idade = 10 + 5 * 2;
+            Console.WriteLine(idade);
+
+            idade = (10 + 5) * 2;
+            Console.WriteLine("Sua idade é " + idade + "!");
+
             Console.WriteLine("Execução finalizada. Tecle Enter para sair...");
             Console.ReadLine();
         }
