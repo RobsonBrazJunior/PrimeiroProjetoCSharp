66fbcf80bee9a1014f4d12ef2aa9bafbb62085f9
Junior Braz <robsonbrazjunior@gmail.com>
2018-09-03 16:57:18 -0400

    Criado segundo projeto - Criando Variaveis

---
+++ 2-CriandoVariaveis/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _2_CriandoVariaveis
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Executando projeto 2 - Criando Variaveis");
+            Console.WriteLine("Execução finalizada. Tecle Enter para sair...");
+            Console.ReadLine();
+        }
+    }
+}
