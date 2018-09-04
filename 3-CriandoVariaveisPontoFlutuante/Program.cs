919f32bc8ddb2db6bb34439449550b6908d01871
Junior Braz <robsonbrazjunior@gmail.com>
2018-09-04 11:27:47 -0400

    Testando variaveis do tipo double

--- 3-CriandoVariaveisPontoFlutuante/Program.cs
+++ 3-CriandoVariaveisPontoFlutuante/Program.cs
@@ -10,6 +10,29 @@ namespace _3_CriandoVariaveisPontoFlutuante
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Executando projeto 3 Criando vaiaveis ponto flutuante");
+
+            double salario;
+            salario = 1200.70;
+
+            Console.WriteLine(salario);
+
+            double idade;
+            idade = 15 / 2.0;
+            Console.WriteLine(idade);
+
+            idade = 5 / 3;
+            Console.WriteLine("5 / 3 = " + idade);
+
+            idade = 5.0 / 3;
+            Console.WriteLine("5.0 / 3 = " + idade);
+
+            double preco = 5.5;
+            int ingressos = 4;
+            Console.WriteLine(preco * ingressos);
+
+            Console.WriteLine("Execução encerrada. Tecle Enter para sair.");
+            Console.ReadLine();
         }
     }
 }
