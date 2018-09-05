4a036eebd502579485c1eddef428aba86b192733
Junior Braz <robsonbrazjunior@gmail.com>
2018-09-05 08:55:11 -0400

    Teste com Casting de variaveis e outros tipos primitivos

--- 4-ConversoesEOutrosTiposNumericos/Program.cs
+++ 4-ConversoesEOutrosTiposNumericos/Program.cs
@@ -10,6 +10,27 @@ namespace _4_ConversoesEOutrosTiposNumericos
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Executando o projeto 4");
+
+            double salario = 12000.50;
+
+            // O int é um tipo de variável que suporta até 32 bits
+            int salarioInteiro = (int)salario;
+            Console.WriteLine(salarioInteiro);
+                   
+            // O long é uma variável de 64 bits
+            long idade = 1300000000000000000;
+            Console.WriteLine(idade);
+
+
+            // O short é um tipo de variável de 16 bits
+            short quantidadeProdutos = 15000;
+            Console.WriteLine(quantidadeProdutos);
+
+            float altura = 1.80f;
+            Console.WriteLine(altura);
+
+            Console.ReadLine();
         }
     }
 }
