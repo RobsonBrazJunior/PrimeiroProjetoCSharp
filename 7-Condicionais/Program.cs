0001a68a96e8f14a13a45332c6b7ac578a0bd8ac
Junior Braz <robsonbrazjunior@gmail.com>
2018-09-05 15:29:41 -0400

    Testes de condição

--- 7-Condicionais/Program.cs
+++ 7-Condicionais/Program.cs
@@ -10,6 +10,28 @@ namespace _7_Condicionais
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Executando projeto 7 - Condicionais");
+
+            int idadeAluno = 16;
+            int quantidadePessoas = 2;
+
+            if (idadeAluno >= 18)
+            {
+                Console.WriteLine("Aluno possui mais de 18 anos de idade.");
+            }
+            else
+            {
+                if (quantidadePessoas >= 2)
+                {
+                    Console.WriteLine("Aluno não possui mais de 18 anos mas está acompanhado");
+                }
+                else
+                {
+                    Console.WriteLine("Aluno não possui mais de 18 anos.");
+                }                
+            }
+
+            Console.ReadLine();
         }
     }
 }
