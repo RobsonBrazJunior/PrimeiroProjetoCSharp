1b252a7709c837fd8635af86b4fe1a979243376e
Junior Braz <robsonbrazjunior@gmail.com>
2018-09-05 14:53:19 -0400

    Teste com atribuições de variáveis

--- 6-AtribuicoesDeVariaveis/Program.cs
+++ 6-AtribuicoesDeVariaveis/Program.cs
@@ -10,6 +10,17 @@ namespace _6_AtribuicoesDeVariaveis
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Executando projeto 6 - Atribuições de Variáveis");
+
+            int idade = 32;
+            int idadeAluno = idade;
+
+            idade = 20;
+
+            Console.WriteLine(idade);
+            Console.WriteLine(idadeAluno);
+
+            Console.ReadLine();
         }
     }
 }
