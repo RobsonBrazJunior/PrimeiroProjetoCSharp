db162326b84605f34375d71555ed2c15ae4cb395
Junior Braz <robsonbrazjunior@gmail.com>
2018-09-05 14:40:33 -0400

    Teste com Strings e Char

--- 5-CaracteresETextos/Program.cs
+++ 5-CaracteresETextos/Program.cs
@@ -10,6 +10,30 @@ namespace _5_CaracteresETextos
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Executando o projeto 5 - Caracteres e Textos");
+
+            //character
+            char primeiraLetra = 'a';
+            Console.WriteLine(primeiraLetra);
+
+            primeiraLetra = (char)65;
+            Console.WriteLine(primeiraLetra);
+
+            primeiraLetra = (char)(primeiraLetra + 1);
+            Console.WriteLine(primeiraLetra);
+
+            string titulo = "Curso de conceitos básicos de C# " + 2018;
+            //A utilização do @ permite criar uma string com várias linhas, conforme a seguir:
+            string cursosProgramacao = @" - .NET
+                - Java
+                - JavaScript";
+
+            Console.WriteLine(titulo);
+            Console.WriteLine(cursosProgramacao);
+
+            string vazia = "";
+
+            Console.ReadLine();
         }
     }
 }
