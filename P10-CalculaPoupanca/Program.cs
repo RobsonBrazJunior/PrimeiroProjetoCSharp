bad6b609ec26e28b7c00bced893d025b6103eb81
Junior Braz <robsonbrazjunior@gmail.com>
2018-09-06 10:59:18 -0400

    Testando repetições

--- P10-CalculaPoupanca/Program.cs
+++ P10-CalculaPoupanca/Program.cs
@@ -10,6 +10,24 @@ namespace P10_CalculaPoupanca
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Executando Projeto 10 - Calcula Poupança");
+
+            double valorInvestido = 1000;
+            int contadorMes = 1;
+
+            while (contadorMes <= 12)
+            {
+                // 0.36% equivale 0.0036
+                valorInvestido = valorInvestido + valorInvestido * 0.0036;
+                Console.WriteLine("Após " + contadorMes + " mês, você terá R$ " + valorInvestido);
+
+                //contadorMes = contadorMes + 1;
+                //contadorMes += 1;
+                contadorMes++;
+            }
+           
+            
+            Console.ReadLine();
         }
     }
 }
