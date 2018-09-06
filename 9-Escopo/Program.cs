ca0502dc5431f9bf26f927011dd56528117f8828
Junior Braz <robsonbrazjunior@gmail.com>
2018-09-06 09:11:36 -0400

    Testando escopo

--- 9-Escopo/Program.cs
+++ 9-Escopo/Program.cs
@@ -10,6 +10,32 @@ namespace _9_Escopo
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Executando projeto 9 - Escopo");
+
+            int idadeAluno = 18;
+            bool acompanhado = true;
+            string mensagemAdicional;
+
+            if (acompanhado == true)
+            {
+                mensagemAdicional = "Alun o está acompanhado";
+            }
+            else
+            {
+                mensagemAdicional = "Aluno não está acompanhado";
+            }
+
+            if (idadeAluno >= 18 && acompanhado == true)
+            {
+                Console.WriteLine("Acesso ao local permitido");
+                Console.WriteLine(mensagemAdicional);
+            }
+            else
+            {
+                Console.WriteLine("Acesso ao local negado");
+            }
+
+            Console.ReadLine();
         }
     }
 }
