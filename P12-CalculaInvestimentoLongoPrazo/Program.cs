aa39b7836ea9f23d9adec3da2c6e8f7b068ade0e
Junior Braz <robsonbrazjunior@gmail.com>
2018-09-10 15:29:36 -0400

    Testando laço FOR Encadeado

--- P12-CalculaInvestimentoLongoPrazo/Program.cs
+++ P12-CalculaInvestimentoLongoPrazo/Program.cs
@@ -10,6 +10,23 @@ namespace P12_CalculaInvestimentoLongoPrazo
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Executando o projeto 12");
+
+            double valorInvestido = 1000;
+            double fatoRendimento = 1.0036;
+
+            for (int contadorAno = 1; contadorAno <= 5; contadorAno++)
+            {
+                for(int contadorMes = 1; contadorMes <= 12; contadorMes++)
+                {
+                    valorInvestido *= fatoRendimento;
+                }
+                fatoRendimento += 0.0010;
+            }
+
+            Console.WriteLine("Ao termino do investimento você terá R$ " + valorInvestido);
+
+            Console.ReadLine();
         }
     }
 }
